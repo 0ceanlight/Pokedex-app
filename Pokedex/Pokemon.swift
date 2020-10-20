099f24c13402d6bdb998dc1670b638fe7d245484
0ceanlight <imahacubak@gmail.com>
2020-10-20 11:28:10 -0500

    first commit

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Christine Frederick on 10/16/20.
+//
+
+import Foundation
---
+++ Pokedex/PokemonViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonViewController.swift
+//  Pokedex
+//
+//  Created by Christine Frederick on 10/18/20.
+//
+
+import Foundation
